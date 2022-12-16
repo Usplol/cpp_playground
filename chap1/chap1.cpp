39a7c7ac8df68150c9bfb431cc8bd942e3686a19
weiyuanzhu <weiyuan.zhu@mackwell.com>
2022-12-16 13:29:13 +0000

    cout cin

--- chap1/chap1.cpp
+++ chap1/chap1.cpp
@@ -13,9 +13,18 @@ int main()
 {
     int width{5};     // direct brace initialization
     int height = {6}; // copy brace initialization
-    int depth{};
-    [[maybe_unused]] int x {5};
+    int depth{};      // zero initialization
+    int x {};
+    char y {};
 
-    std::cout << "width: " << width << " height: " << height << " depth: " << depth << std::endl;
+    std::cout << "width: " << width << '\n' << "height: " << height << " depth: " << depth << std::endl;
+    std::cout << "enter a number for x" << std::endl;
+    std::cin >> x;
+    std::cout << "enter a char for y" << std::endl;
+    std::cin >> y;
+
+    std::cout << "X entered: " << x <<", y entered: " << y << std::endl;
+
+    
     return 0;
 }
